19b78718bb94545357bac76209627cd5c7c7af27
SumitAnglekar94 <anglekar.s@northeastern.edu>
2019-09-22 18:55:36 -0400

    Created basic model and implemented user POST

---
+++ webapp/recipe/src/main/java/com/cloud/ccwebapp/recipe/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.cloud.ccwebapp.recipe.controller;
+
+import com.cloud.ccwebapp.recipe.model.User;
+import com.cloud.ccwebapp.recipe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(method= RequestMethod.POST, value="/user")
+    public User addTopic(@RequestBody User user) {
+       return userRepository.save(user);
+    }
+
+}
