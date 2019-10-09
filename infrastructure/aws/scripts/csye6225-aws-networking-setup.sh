49d3f85160a7f3a754a15872dce8db8146c75239
Tejas Shah <shah.te@husky.neu.edu>
2019-10-08 23:58:25 -0400

    Changes to create script

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -5,7 +5,7 @@ aws_region=${1:-"us-east-1"}
 vpc_cidr_block=${2:-"10.0.0.0/16"}
 subnet_cidr_block1=${3:-"10.0.1.0/24"}
 subnet_cidr_block2=${4:-"10.0.2.0/24"}
-subnet_cidr_block3=${5:-"10.0.3.2/24"}
+subnet_cidr_block3=${5:-"10.0.3.0/24"}
 vpc_name=${6:-"NEW_VPC"}
 
 # Run create-vpc command
@@ -48,6 +48,7 @@ aws ec2 create-tags --resources "$vpc_id" --tags Key=Name,Value="$vpc_name"
                             --vpc-id "${vpc_id}" \
                             --output json)"
     subnet_id1=$(echo "$subnet1" | /usr/bin/jq '.Subnet.SubnetId' | tr -d '"')
+    aws ec2 create-tags --resources "$subnet_id1" --tags Key=Name,Value="${vpc_name}_subnet1"
     echo "Created subnet: ${subnet_id1}"
 
     subnet2="$(aws ec2 create-subnet --availability-zone-id "${subnet_avbl_zone2}" \
@@ -55,6 +56,7 @@ aws ec2 create-tags --resources "$vpc_id" --tags Key=Name,Value="$vpc_name"
                             --vpc-id "${vpc_id}" \
                             --output json)"
     subnet_id2=$(echo "$subnet2" | /usr/bin/jq '.Subnet.SubnetId' | tr -d '"')
+    aws ec2 create-tags --resources "$subnet_id2" --tags Key=Name,Value="${vpc_name}_subnet2"
     echo "Created subnet: ${subnet_id2}"
     
     subnet3="$(aws ec2 create-subnet --availability-zone-id "${subnet_avbl_zone3}" \
@@ -62,6 +64,7 @@ aws ec2 create-tags --resources "$vpc_id" --tags Key=Name,Value="$vpc_name"
                             --vpc-id "${vpc_id}" \
                             --output json)"
     subnet_id3=$(echo "$subnet3" | /usr/bin/jq '.Subnet.SubnetId' | tr -d '"')
+    aws ec2 create-tags --resources "$subnet_id3" --tags Key=Name,Value="${vpc_name}_subnet3"
     echo "Created subnet: ${subnet_id3}"
 } || {
     echo "Unable to create subnets"
