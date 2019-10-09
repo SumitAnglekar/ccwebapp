97298cd460683d828f34c08df2b90e8c73de7975
tejas.shah <tejas.shah@attivio.com>
2019-10-09 17:43:12 -0400

    Implement remaining CLI creation script

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -8,24 +8,34 @@ subnet_cidr_block2=${4:-"10.0.2.0/24"}
 subnet_cidr_block3=${5:-"10.0.3.0/24"}
 vpc_name=${6:-"NEW_VPC"}
 
+export AWS_DEFAULT_REGION=$aws_region
+
+# Check if AWS_PROFILE is set?
+if [ -z "$AWS_PROFILE" ]
+then
+    echo "Please set the AWS_PROFILE first!"
+    exit 1
+fi
+
+# Check if VPC with the name already exists?
+existing_vpc_id=$(aws ec2 describe-tags --filter Name=tag:Name,Values=${vpc_name} | /usr/bin/jq '.Tags[0].ResourceId' | tr -d '"')
+if [ -z "$existing_vpc_id" ] || [ "$existing_vpc_id" = "null" ]
+then
+    echo "Creating a new VPC..."
+else
+    echo "VPC with Name ${vpc_name} already exists!"
+    exit 1
+fi
+
 # Run create-vpc command
 {
-    script_response="$(aws ec2 create-vpc --cidr-block "${vpc_cidr_block}" --output json)"
+    vpc_id="$(aws ec2 create-vpc --cidr-block "${vpc_cidr_block}" --output json | /usr/bin/jq '.Vpc.VpcId' | tr -d '"')"
+    # Add the name tag to the VPC
+    aws ec2 create-tags --resources "$vpc_id" --tags Key=Name,Value="$vpc_name"
 } || {
     echo "VPC creation failed"
     exit 1
 }
-# Fetch the VPC ID
-{
-    vpc_id=$(echo "$script_response" | /usr/bin/jq '.Vpc.VpcId' | tr -d '"')
-} || {
-    echo "Unable to fetch VPC Id"
-    exit 1
-}
-echo "VPC with VPC ID: $vpc_id created"
-
-# Add the name tag to the VPC
-aws ec2 create-tags --resources "$vpc_id" --tags Key=Name,Value="$vpc_name"
 
 # TODO
 # Create subnets in your VPC. You must create 3 subnets, each in different availability zone
@@ -43,45 +53,69 @@ aws ec2 create-tags --resources "$vpc_id" --tags Key=Name,Value="$vpc_name"
 
 # Step 2: Create the subnets
 {
+    echo "Creating subnet1..."
     subnet1="$(aws ec2 create-subnet --availability-zone-id "${subnet_avbl_zone1}" \
                             --cidr-block "${subnet_cidr_block1}" \
                             --vpc-id "${vpc_id}" \
                             --output json)"
     subnet_id1=$(echo "$subnet1" | /usr/bin/jq '.Subnet.SubnetId' | tr -d '"')
     aws ec2 create-tags --resources "$subnet_id1" --tags Key=Name,Value="${vpc_name}_subnet1"
-    echo "Created subnet: ${subnet_id1}"
 
+    echo "Creating subnet2..."
     subnet2="$(aws ec2 create-subnet --availability-zone-id "${subnet_avbl_zone2}" \
                             --cidr-block "${subnet_cidr_block2}" \
                             --vpc-id "${vpc_id}" \
                             --output json)"
     subnet_id2=$(echo "$subnet2" | /usr/bin/jq '.Subnet.SubnetId' | tr -d '"')
     aws ec2 create-tags --resources "$subnet_id2" --tags Key=Name,Value="${vpc_name}_subnet2"
-    echo "Created subnet: ${subnet_id2}"
     
+    echo "Creating subnet3..."
     subnet3="$(aws ec2 create-subnet --availability-zone-id "${subnet_avbl_zone3}" \
                             --cidr-block "${subnet_cidr_block3}" \
                             --vpc-id "${vpc_id}" \
                             --output json)"
     subnet_id3=$(echo "$subnet3" | /usr/bin/jq '.Subnet.SubnetId' | tr -d '"')
     aws ec2 create-tags --resources "$subnet_id3" --tags Key=Name,Value="${vpc_name}_subnet3"
-    echo "Created subnet: ${subnet_id3}"
 } || {
     echo "Unable to create subnets"
     exit 1
 }
 
-
-# TODO
 # Create Internet Gateway resource. and attach the Internet Gateway to the VPC.
+{
+    echo "Creating Internet Gateway..."
+    ig_id=$(aws ec2 create-internet-gateway | /usr/bin/jq '.InternetGateway.InternetGatewayId' | tr -d '"')
+    aws ec2 create-tags --resources "$ig_id" --tags Key=Name,Value="${vpc_name}_internet_gateway"
+    echo "Attaching the Internet Gateway to the VPC..."
+    aws ec2 attach-internet-gateway --internet-gateway-id "$ig_id" --vpc-id "$vpc_id"
+} || {
+    echo "Error while Internet Gateway creation"
+    exit 1
+}
 
-
-# TODO
 # Create a public route table. Attach all subnets created above to the route table.
+{
+    echo "Creating a public route table..."
+    route_table_id=$(aws ec2 create-route-table --vpc-id "${vpc_id}" | /usr/bin/jq '.RouteTable.RouteTableId' | tr -d '"')
+    aws ec2 create-tags --resources "$route_table_id" --tags Key=Name,Value="${vpc_name}_route_table"
+    echo "Attaching subnet 1 to route table..."
+    aws ec2 associate-route-table --route-table-id "${route_table_id}" --subnet-id "${subnet_id1}" >/dev/null
+    echo "Attaching subnet 2 to route table..."
+    aws ec2 associate-route-table --route-table-id "${route_table_id}" --subnet-id "${subnet_id2}" >/dev/null
+    echo "Attaching subnet 3 to route table..."
+    aws ec2 associate-route-table --route-table-id "${route_table_id}" --subnet-id "${subnet_id3}" >/dev/null
+} || {
+    echo "Error while creating route table."
+    exit 1
+}
 
-
-# TODO
 # Create a public route in the public route table created above with 
 # destination CIDR block 0.0.0.0/0 and internet gateway created above as the target.
+{
+    echo "Creating a public route..."
+    aws ec2 create-route --route-table-id "${route_table_id}" --destination-cidr-block "0.0.0.0/0" --gateway-id "${ig_id}" >/dev/null
+} || {
+    echo "Error creating public route"
+}
 
-
+echo "AWS setup via CLI successful!"
