fb9446b6f9987d3bdabf5dbb1623b02d0647391a
SumitAnglekar94 <anglekar.s@northeastern.edu>
2019-10-22 21:15:47 -0400

    deleted the module folder

--- infrastructure/aws/terraform/module/main.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
- 
-  source = "../"
-  vpcCidrBlock = "${var.vpcCidrBlock}"
-  vpcName = "${var.vpcName}"
-  subnetCidrBlock = "${var.subnetCidrBlock}"
-  rdsPassword = "${var.rdsPassword}"
-}
