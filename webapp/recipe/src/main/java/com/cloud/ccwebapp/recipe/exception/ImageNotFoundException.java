183a17591a4ea16a852a7621415b6380d1d2445a
tejas.shah <tejas.shah@attivio.com>
2019-10-24 15:39:26 -0400

    Adding s3 object metadata

--- webapp/recipe/src/main/java/com/cloud/ccwebapp/recipe/exception/ImageNotFoundException.java
+++ webapp/recipe/src/main/java/com/cloud/ccwebapp/recipe/exception/ImageNotFoundException.java
@@ -4,7 +4,7 @@ package com.cloud.ccwebapp.recipe.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ImageNotFoundException extends RuntimeException {
     public ImageNotFoundException(String message) {
         super(message);
