eb6b76300b54bcaef1ed3cab82b80b2673e22cc0
Tejas Shah <shah.te@husky.neu.edu>
2019-10-24 02:23:13 -0400

    changes to aws instance script

--- infrastructure/aws/terraform/modules/application/prepare_aws_instance.sh
+++ infrastructure/aws/terraform/modules/application/prepare_aws_instance.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 sudo echo 'export JAVA_OPTS="-Daws.s3.bucketname='${s3_bucket_name}'"' > /opt/tomcat/bin/setenv.sh
-sudo echo 'JAVA_OPTS="$JAVA_OPTS: -Dspring.datasource.url=jdbc:postgresql://'${aws_db_endpoint}'/'${aws_db_name}'"' >> /opt/tomcat/bin/setenv.sh
-sudo echo 'JAVA_OPTS="$JAVA_OPTS: -Dspring.datasource.username='${aws_db_username}'"' >> /opt/tomcat/bin/setenv.sh
-sudo echo 'JAVA_OPTS="$JAVA_OPTS: -Dspring.datasource.password='${aws_db_password}'"' >> /opt/tomcat/bin/setenv.sh
-sudo echo 'JAVA_OPTS="$JAVA_OPTS: -Daws.region='${aws_region}'"' >> /opt/tomcat/bin/setenv.sh
-sudo echo 'JAVA_OPTS="$JAVA_OPTS: -Daws.profile='${aws_profile}'"' >> /opt/tomcat/bin/setenv.sh
+sudo echo 'JAVA_OPTS="$JAVA_OPTS -Dspring.datasource.url=jdbc:postgresql://'${aws_db_endpoint}'/'${aws_db_name}'"' >> /opt/tomcat/bin/setenv.sh
+sudo echo 'JAVA_OPTS="$JAVA_OPTS -Dspring.datasource.username='${aws_db_username}'"' >> /opt/tomcat/bin/setenv.sh
+sudo echo 'JAVA_OPTS="$JAVA_OPTS -Dspring.datasource.password='${aws_db_password}'"' >> /opt/tomcat/bin/setenv.sh
+sudo echo 'JAVA_OPTS="$JAVA_OPTS -Daws.region='${aws_region}'"' >> /opt/tomcat/bin/setenv.sh
+sudo echo 'JAVA_OPTS="$JAVA_OPTS -Daws.profile='${aws_profile}'"' >> /opt/tomcat/bin/setenv.sh
