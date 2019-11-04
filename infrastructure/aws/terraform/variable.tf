05b4467074f6f6362144c250e0bf2eef0fa69684
Tejas Shah <shah.te@husky.neu.edu>
2019-11-04 14:39:13 -0500

    Merged the CI-CD module into application module

--- infrastructure/aws/terraform/variable.tf
+++ infrastructure/aws/terraform/variable.tf
@@ -76,25 +76,3 @@ variable "aws_ssh_key" {
   type = "string"
   description = "The ssh key pair name configured in AWS"
 }
-
-#CI-CD
-
-variable "compute_platform" {
-   type      = "string"
-}
-
-variable "app_name" {
-   type      = "string"
-}
-
-variable "deployment_group_name" {
-   type      = "string"
-}
-
-variable "deployment_config_name" {
-   type      = "string"
-}
-
-variable "service_role" {
-   type      = "string"
-}
