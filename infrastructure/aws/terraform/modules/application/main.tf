27841aabbf65b49461a1564fa30dea5bd813f6ee
SumitAnglekar94 <anglekar.s@northeastern.edu>
2019-11-10 18:43:09 -0500

    Created lambda function along with it's subscription,roles and permissions

--- infrastructure/aws/terraform/modules/application/main.tf
+++ infrastructure/aws/terraform/modules/application/main.tf
@@ -464,6 +464,53 @@ data "aws_iam_policy_document" "sns-topic-policy" {
   }
 }
 
+#Lambda Function
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+  runtime       = "java8"
+  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+}
+
+#SNS topic subscription to Lambda
+resource "aws_sns_topic_subscription" "lambda" {
+  topic_arn = "${aws_sns_topic.test.arn}"
+  protocol  = "lambda"
+  endpoint  = "${aws_lambda_function.test_lambda.arn}"
+}
+
+#SNS Lambda permission
+resource "aws_lambda_permission" "with_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.test_lambda.function_name}"
+  principal     = "sns.amazonaws.com"
+  source_arn    = "${aws_sns_topic.test.arn}"
+}
+
+#IAM Role for lambda with sns
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 # IAM Role for CodeDeploy
 resource "aws_iam_role" "code_deploy_role" {
   name = "CodeDeployServiceRole"
