6f0644d9c35efea6ac81eae04590c572456565dd
Ishita Sequeira <sequeira.i@northeastern.edu>
2019-11-17 02:38:16 -0500

    made changes in the security groups

--- infrastructure/aws/terraform/modules/application/main.tf
+++ infrastructure/aws/terraform/modules/application/main.tf
@@ -207,9 +207,9 @@ resource "aws_security_group" "application" {
     cidr_blocks  = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks  = ["0.0.0.0/0"]
   }
   tags          = {
